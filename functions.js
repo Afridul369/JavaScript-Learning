21920fb5484efb25dc0bf7ae40526aba1911ca05
Afridul369 <afridul.dev@gmail.com>
2025-04-19 16:01:46 +0600

    function_1 done

---
+++ functions.js
@@ -0,0 +1,24 @@
+function nums (num1,num2){
+   let  num3 = num1 + num2
+   return num3
+}
+// console.log(nums(2,3));  // 5
+
+function nums (num1,num2){
+//    console.log(num1 + num2);
+   
+ }
+nums(2,3); // 5
+nums(2,'3'); // 23
+nums(2,null); // 2
+nums(2,'null'); // 2null
+
+function login (name){
+    return ` My name is ${name}`
+}
+// login('abir') // function execute hoise
+// console.log(login('abir')); // My name is abir
+
+
+
+
