d3ecffa54e3679eae149f7a6d2d20804a3525053
Afridul369 <afridul.dev@gmail.com>
2025-04-20 16:23:17 +0600

    scope done

---
+++ scope.js
@@ -0,0 +1,16 @@
+// Scope level & Hoisting
+function abir (){
+        const one = 'this is one'
+        console.log("Hello Abir"); // Hello Abir
+            function amit (){
+                const two = 'this is two'
+                console.log("Hello Amit"); // Hello Amit
+    }
+        // console.log(two); // eta execute hbe na karone scope r baire two k call disi tai
+        
+    amit()
+    console.log(one); // this is one
+    
+}
+// console.log(two); // eta execute hbe na karone scope r baire two k call disi tai
+abir()
